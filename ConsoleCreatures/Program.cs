b35acb525634fc6afef73b4f47b7c81c0c715444
ricardocarva <ricardofecarva@gmail.com>
2022-03-12 18:16:28 -0500

    Add project files.

---
+++ ConsoleCreatures/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            //The purpose of this program is to create a simple console creature
+            Console.WriteLine("        .-.");
+            Console.WriteLine("       (o o)");
+            Console.WriteLine("      -| O |-");
+            Console.WriteLine("       |   |");
+            Console.WriteLine("       |   |");
+            Console.WriteLine("       |   |");
+            Console.WriteLine("       '~~~' ");
+            Console.WriteLine("       /   \\");
+
+            Console.WriteLine("This is Steve, the ghoul!" + "\r\n\r\n" + "Press enter to end the program");
+
+            //Press enter to end program
+            Console.ReadLine( );
+
+        }
+    }
+}
